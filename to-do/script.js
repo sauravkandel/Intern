2f97037567d8d96b2ae96c40b6d45ec1c43fef27
sauravkandel <34673190+sauravkandel@users.noreply.github.com>
2023-03-12 21:58:18 +0545

    Update script.js

--- to-do/script.js
+++ to-do/script.js
@@ -15,10 +15,21 @@ document.querySelector('#push').onclick = function(){
             </div>
         `;
 
+         // Store the task on local storage
+         var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+         tasks.push(document.querySelector('#newtask input').value);
+         localStorage.setItem('tasks', JSON.stringify(tasks));
+
         var current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
+
+        // Remove the task from local storage
+        var taskName = this.parentNode.querySelector('#taskname').textContent;
+        var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.splice(tasks.indexOf(taskName), 1);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
             }
         }
     }
